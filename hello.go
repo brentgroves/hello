dbac327fc33d663d60e217c8d65c0fbfa00f3c56
brentgroves <brent.groves@gmail.com>
2023-09-19 18:23:38 -0400

    updated

--- hello.go
+++ hello.go
@@ -3,10 +3,13 @@ package main
 import (
 	"fmt"
 
-	"brentgroves/greetings"
+	"github.com/brentgroves/greetings"
+
+	"rsc.io/quote"
 )
 
 func main() {
+	fmt.Println(quote.Go())
 	// Get a greeting message and print it.
 	message := greetings.Hello("Gladys")
 	fmt.Println(message)
